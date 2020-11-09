c6b9fb8dabc691edbdcf8af694dda8af643a3430
Kittisak <dnjooiopa@gmail.com>
2020-11-09 15:27:20 +0700

    paint ball

--- TouchGFX/gui/src/screen_screen/screenView.cpp
+++ TouchGFX/gui/src/screen_screen/screenView.cpp
@@ -1,6 +1,7 @@
 #include <gui/screen_screen/screenView.hpp>
 #include <touchgfx/Color.hpp>
 #include "BitmapDatabase.hpp"
+//#include "touchgfx/lcd/LCD.hpp"
 
 screenView::screenView()
 {
@@ -19,7 +20,14 @@ void screenView::tearDownScreen()
 void  screenView::move_up(){
 	if(ball.getY() < 0 || ball.getY() > 150)
 		direction = -direction;
+
+	ballPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(255, 255, 255));
+	ball.setPainter(ballPainter);
+	ball.invalidate();
+
 	ball.setY(ball.getY()+direction);
+	ballPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(250, 0, 0));
+	ball.setPainter(ballPainter);
 	ball.invalidate();
 
 }
